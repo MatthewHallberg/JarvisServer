3f767cb92fb730ea45fe92161b4f13e8dbeca6ef
Matthew Hallberg <matthewhallberg@gmail.com>
2019-02-01 04:28:54 -0500

    sending to alexa, not yet recieving

--- index.js
+++ index.js
@@ -1,76 +1,28 @@
-var https = require('https')
+const express = require('express');
+const app = express();
+var http = require("http");
+var https = require("https");
 
-exports.handler = (event, context) => {
+//run instructions:
+//lt --port 3000 &
+//disown
 
-  try {
 
-    if (event.session.new) {
-      // New Session
-      console.log("NEW SESSION")
-    }
+app.post('/', (req, res) =>
 
-    switch (event.request.type) {
+    res.send({
+        version: '1.0',
+        response: {
+        shouldEndSession: false,
+        outputSpeech: {
+            type: 'SSML',
+            text: 'Hello World!',
+            ssml: '<speak>matt is the best</speak>'
+            }
+        }
+    }));
 
-      case "LaunchRequest":
-        // Launch Request
-        console.log(`LAUNCH REQUEST`)
-          context.succeed(
-            generateResponse(
-             buildSpeechletResponse("Jarvis initialized", false),
-             {}
-            )
-          )
-        break;
-        
-      case "IntentRequest":
-        // Intent Request
-        console.log(`INTENT REQUEST`)
-        var intent = event.request.intent;
-        //context.succeed(generateResponse(buildSpeechletResponse(intent.name, false),{}))
-        switch(intent.name){
-          case "YouTubeVideo":
-             context.succeed(generateResponse(buildSpeechletResponse("Jarvis YouTube", false),{}))
-             break;
-          case "Display":
-             var displayNum = intent.slots.displayNum.value
-             context.succeed(generateResponse(buildSpeechletResponse('Jarvis Selecting display ' + displayNum, false),{}))
-             break;
-          case "AMAZON.PlaybackAction<object@MusicCreativeWork>":
-             context.succeed(generateResponse(buildSpeechletResponse('Jarvis Music', false),{}))
-             break;
-          default:
-            context.succeed(generateResponse(buildSpeechletResponse("Jarvis Command not Found", false),{}))
-          }
-        break;
-        
-      case "SessionEndedRequest":
-        // Session Ended Request
-        console.log(`SESSION ENDED REQUEST`)
-        break;
 
-      default:
-        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
 
-    }
 
-  } catch(error) { context.fail(`Exception: ${error}`) }
-
-}
-
-// Helpers
-var buildSpeechletResponse = (outputText, shouldEndSession) => {
-  return {
-    outputSpeech: {
-      type: "PlainText",
-      text: outputText
-    },
-    shouldEndSession: shouldEndSession
-  }
-}
-var generateResponse = (speechletResponse, sessionAttributes) => {
-  return {
-    version: "1.0",
-    sessionAttributes: sessionAttributes,
-    response: speechletResponse
-  }
-}
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
